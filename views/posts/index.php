2b4d122109b9d220cb71be3a0b5e322be5a4ec57
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2017-09-08 18:26:38 +0200

    delete some view files creating for test

--- views/posts/index.php
+++
@@ -1,8 +0,0 @@
-<p>Here is a list of all posts:</p>
-
-<?php foreach($chapters as $chapter) { ?>
-  <p>
-    <?php echo $chapter->getAuthor(); ?>
-    <a href='?controller=posts&action=show&id=<?php echo $chapter->getId(); ?>'>See content</a>
-  </p>
-<?php } ?>
--- views/posts/show.php
+++
@@ -1,6 +0,0 @@
-<p>This is the requested post:</p>
-
-<p><?php echo $chapter->getAuthor(); ?></p>
-<p><?php echo $chapter->getTitle(); ?></p>
-<p><?php echo $chapter->getContent(); ?></p>
-<p><?php echo $chapter->getDate(); ?></p>
